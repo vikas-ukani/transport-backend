ebe8ab4020846afb8654568170318c6c1a22b6c0
Vikas Ukani <vikasukani5@gmail.com>
2025-12-01 19:55:34 +0530

    update port

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ if __name__ == '__main__':
     # Run the Prisma generate command
     subprocess.run(["prisma", "generate"], check=True)
     # Start the FastAPI server
-    uvicorn.run('app.main:app', host="127.0.0.1", reload=True, lifespan="on", port=8001)
+    uvicorn.run('app.main:app', host="0.0.0.0", reload=True, lifespan="on", port=8001)
